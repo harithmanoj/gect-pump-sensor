8c747d740f949e3721c8c04df58b0e36b24bfd00
Harith Manoj <harithpub@gmail.com>
2021-12-05 17:17:28 +0530

    move DataFormats to another python file

--- GECT ECE Pump Pressure Sensor Module/plot/plotWindow.py
+++ GECT ECE Pump Pressure Sensor Module/plot/plotWindow.py
@@ -15,6 +15,8 @@ NavigationToolbar2Tk)
 
 import tkinter as tk
 
+from DataForms import *
+
 def loadData():
 
     files = []
@@ -23,97 +25,6 @@ def loadData():
         files.append(path.name)
 
     return files
-
-class GraphData:
-    
-    def __init__(self, axsFrom: float, axsTo: float):
-
-        self.axesFrom = axsFrom
-        self.axesTo = axsTo
-
-        self.value = []
-
-    def update(self, value: float):
-        raise NotImplementedError()
-
-    def getData(self):
-        return self.value
-
-    def getLength(self):
-        return len(self.value)
-
-    def getValue(self, i):
-        return self.value[i]
-
-    def top(self):
-        if(len(self.value) == 0):
-            return 0.0
-        else:
-            return self.value[self.getLength() - 1]
-
-    def getName(self):
-        raise NotImplementedError()
-
-class RawData(GraphData):
-
-    def __init__(self, axsFrom: float, axsTo: float):
-
-        super().__init__(axsFrom, axsTo)
-
-    def update(self, value: float):
-        self.value.append(value * self.axesTo / self.axesFrom)
-
-    def getName(self):
-        return "Raw Data"
-
-class MovingAverage(GraphData):
-
-    def __init__(self, axsFrom: float, axsTo: float, averageSize: int):
-
-        self.averagingSize = averageSize
-        self.sum: float = 0.0
-
-        super().__init__(axsFrom, axsTo)
-
-    def update(self, value: float):
-
-        if(len(self.value) >= (self.averagingSize)):
-            self.sum -= self.value[len(self.value) - self.averagingSize]
-            self.sum += value
-            self.value.append(self.sum / self.averagingSize)
-        else:
-            self.sum += value
-            self.value.append(self.sum / (len(self.value)+ 1 ))
-
-    def getName(self):
-        return "Moving Average"
-
-class BlockedAverage(GraphData):
-
-    def __init__(self, axsFrom: float, axsTo: float, averagingSize: int) -> None:
-        
-        self.averagingSize = averagingSize
-        self.sum: float = 0.0
-        self.last: float = 0.0
-        self.count: int = 0
-
-        super().__init__(axsFrom, axsTo)
-
-    def update(self, value: float):
-
-        self.sum += value
-        self.count += 1
-        
-        if(self.count == self.averagingSize):
-            self.last = self.sum / self.averagingSize
-            self.sum = 0.0
-            self.count = 0
-        
-        self.value.append(self.last)
-
-    def getName(self):
-        return "Blocked Average"
-    
 
 def read(file):
 
