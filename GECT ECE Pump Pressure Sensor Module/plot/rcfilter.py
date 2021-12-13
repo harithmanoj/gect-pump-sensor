208a3c115ec9a585913fa25485df3f26e7b2c4b2
Harith Manoj <harithpub@gmail.com>
2021-12-13 14:14:56 +0530

    plot rc filter freq chara

---
+++ GECT ECE Pump Pressure Sensor Module/plot/rcfilter.py
@@ -0,0 +1,28 @@
+
+
+import matplotlib.pyplot as plt
+import math
+
+
+
+
+def main():
+
+    value = ([], [])
+
+    R = 47000
+    C = 1 * (10**-6)
+
+    fc = 1 / (2 * math.pi * R *C)
+    print(fc)
+
+    for i in range(1, 7000):
+        x = float(i) / 1000.0
+        value[0].append((x))
+        value[1].append(fc / math.sqrt((fc*fc) + (x*x)))
+    
+    plt.plot(value[0], value[1])
+    plt.show()
+
+if __name__ == "__main__":
+    main()
