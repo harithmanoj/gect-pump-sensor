4f935fa0bdfd56bd23b1be08d736cc747d4ab79e
Harith Manoj <harithpub@gmail.com>
2021-12-14 16:19:35 +0530

    ltspice

--- GECT ECE Pump Pressure Sensor Module/plot/pwlexport.py
+++ GECT ECE Pump Pressure Sensor Module/plot/pwlexport.py
@@ -17,6 +17,14 @@ def read(file):
 
     return data
 
+def readPWL(file):
+
+    f = open(file, "r", encoding = 'utf-8')
+
+    data = []
+
+    for line
+
 def main():
 
     file = input("File name :")
