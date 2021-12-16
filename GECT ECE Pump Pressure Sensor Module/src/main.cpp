1725d70cce047d581dd997344010c98d2b9ca9af
Harith Manoj <harithpub@gmail.com>
2021-12-16 20:42:02 +0530

    add convert to string etc

--- GECT ECE Pump Pressure Sensor Module/src/main.cpp
+++ GECT ECE Pump Pressure Sensor Module/src/main.cpp
@@ -3,21 +3,44 @@
 
 constexpr uint8_t pressurePin = A0;
 
-bool led = true;
+auto led = HIGH;
 int count = 0;
+
+constexpr unsigned long frequency = 40;
+constexpr unsigned long time_delay = 1000 / frequency;
+
+
+char writeString[9] = "";
+
+inline void writeToGlobalString(int analog, int val1, int val2)
+{
+  writeString[4] = ' ';
+  writeString[6] = ' ';
+  writeString[8] = '\0';
+
+  writeString[0] = '0' + (analog / 1000);
+  writeString[1] = '0' + ((analog / 100) % 10);
+  writeString[2] = '0' + ((analog / 10) % 10);
+  writeString[3] = '0' + (analog % 10);
+
+  writeString[5] = '0' + val1;
+  writeString[7] = '0' + val2;
+}
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(BUILTIN_LED, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(BUILTIN_LED, (led ? HIGH : LOW));
+  digitalWrite(LED_BUILTIN, led);
+
   Serial.println(analogRead(pressurePin));
-  led = ((count % 40) > 28 ? true : false);
+
+  led = ((count % 40) > 28 ? HIGH : LOW);
   ++count;
-  delay(25);
   
+  delay(time_delay);  
 }
