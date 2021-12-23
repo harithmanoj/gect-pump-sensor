66fce7064f40e53e2a41ef68f39a3395f25f2430
Harith Manoj <harithpub@gmail.com>
2021-12-23 15:19:37 +0530

    Re structure

---
+++ plot/graphCanvasWindow.py
@@ -0,0 +1,21 @@
+# Graph Window single
+
+
+# (C) Harith Manoj 2021
+
+
+
+
+from matplotlib.figure import Figure
+from scrollableSourceList import SourceList
+from plotSettings import PlotSettings
+import pathlib
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
+NavigationToolbar2Tk)
+
+class graphWindow:
+
+
+    def __init__(self) -> None:
+        pass
